c3d6b15a6dcf3699817d2680aa241a38f55391fc
Christopher Madden <maddenc33@gmail.com>
2022-03-23 14:26:16 -0400

    Adding the election audit code.

---
+++ Python_practice.py
@@ -0,0 +1,12 @@
+counties = ['Arapahoe', 'Denver','Jefferson']
+if "Arapahoe" in counties and "El Paso" not in counties:
+   print("Only Arapahoe is in the list of counties.")
+else:
+    print("Arapahoe is in the list of counties and El Paso is not in the list of counties.")
+for i in range(len(counties)):
+    print(counties[i])
+counties_tuple = ('Arapahoe','Denver','Jefferson') 
+for i in range(len(counties_tuple)):
+
+      print(counties_tuple[i])
+
